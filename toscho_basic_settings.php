d1d736135ebe6df9b1a009afbdf0c67e214f5f2f
toscho <info@toscho.e>
2010-01-30 01:58:46 +0100

    Initial public release.
    Signed-off-by: toscho <info@toscho.e>

---
+++ toscho_basic_settings.php
@@ -0,0 +1,36 @@
+<?php
+/*
+Plugin Name: Toscho's basic settings
+Plugin URI: http://toscho.de/
+Description: Some useful default configuration settings. See 'wp-admin/options.php' for more options.
+Version: 0.2
+Author: Thomas Scholz
+Author URI: http://toscho.de
+*/
+
+function set_toscho_defaults()
+{
+	$o = array(
+		'avatar_default'			=> 'blank',
+		'avatar_rating'				=> 'G',
+		'category_base'				=> '/thema',
+		'comment_max_links'			=> 0,
+		'comments_per_page'			=> 0,
+		'date_format'				=> 'd.m.Y',
+		'default_ping_status'		=> 'closed',
+		'default_post_edit_rows'	=> 30,
+		'links_updated_date_format'	=> 'j. F Y, H:i',
+		'permalink_structure'		=> '/%year%/%postname%/',
+		'rss_language'				=> 'de',
+		'timezone_string'			=> 'Etc/GMT-1',
+		'use_smilies'				=> 0,	
+	);
+
+	foreach ( $o as $k => $v )
+	{
+		update_option($k, $v);
+	}
+
+	return;
+}
+register_activation_hook(__FILE__, 'set_toscho_defaults');
