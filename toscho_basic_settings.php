2e2a18125b691d8b19350a57996d299fbab6ba7e
toscho <info@toscho.e>
2010-02-15 18:54:58 +0100

    Changed tabs to whitespace for better Github compatibility …
    Signed-off-by: toscho <info@toscho.e>

--- toscho_basic_settings.php
+++ toscho_basic_settings.php
@@ -1,46 +1,46 @@
 <?php
 /*
 Plugin Name: Toscho's basic settings
-Plugin URI: http://toscho.de/
-Description: Some useful default configuration settings. See 'wp-admin/options.php' for more options.
-Version: 0.3
-Author: Thomas Scholz
-Author URI: http://toscho.de
+Plugin URI:  http://toscho.de/2010/wordpress-grundeinstellungen-per-plugin-setzen/
+Description: Some useful default configuration settings. See 'wp-admin/options.php' for more options. 
+Version:     0.3
+Author:      Thomas Scholz
+Author URI:  http://toscho.de
 */
 
 function set_toscho_defaults()
 {
-	global $wpdb;
-	
-	$o = array(
-		'avatar_default'			=> 'blank',
-		'avatar_rating'				=> 'G',
-		'category_base'				=> '/thema',
-		'comment_max_links'			=> 0,
-		'comments_per_page'			=> 0,
-		'date_format'				=> 'd.m.Y',
-		'default_ping_status'		=> 'closed',
-		'default_post_edit_rows'	=> 30,
-		'links_updated_date_format'	=> 'j. F Y, H:i',
-		'permalink_structure'		=> '/%year%/%postname%/',
-		'rss_language'				=> 'de',
-		'start_of_week'				=> 1,
-		'timezone_string'			=> 'Etc/GMT-1',
-		'use_smilies'				=> 0,	
-	);
+    global $wpdb;
 
-	foreach ( $o as $k => $v )
-	{
-		update_option($k, $v);
-	}
-	
-	// Delete dummy post and comment.
-	wp_delete_post(1, TRUE);
-	wp_delete_comment(1);
-	
-	// empty blogroll
-	$wpdb->query("DELETE FROM $wpdb->links WHERE link_id != ''");
+    $o = array(
+        'avatar_default'            => 'blank',
+        'avatar_rating'             => 'G',
+        'category_base'             => '/thema',
+        'comment_max_links'         => 0,
+        'comments_per_page'         => 0,
+        'date_format'               => 'd.m.Y',
+        'default_ping_status'       => 'closed',
+        'default_post_edit_rows'    => 30,
+        'links_updated_date_format' => 'j. F Y, H:i',
+        'permalink_structure'       => '/%year%/%postname%/',
+        'rss_language'              => 'de',
+        'start_of_week'             => 1,
+        'timezone_string'           => 'Etc/GMT-1',
+        'use_smilies'               => 0
+    );
 
-	return;
+    foreach ( $o as $k => $v )
+    {
+        update_option($k, $v);
+    }
+
+    // Delete dummy post and comment.
+    wp_delete_post(1, TRUE);
+    wp_delete_comment(1);
+
+    // empty blogroll
+    $wpdb->query("DELETE FROM $wpdb->links WHERE link_id != ''");
+
+    return;
 }
 register_activation_hook(__FILE__, 'set_toscho_defaults');
